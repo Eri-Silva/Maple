a5e20a6ebbda37b1510ba50936b54037ec9d2ebb
ERIELSON BRITO DA SILVA <silva.erielson@academico.ifrn.edu.br>
2022-08-17 10:45:51 -0300

    meu primeiro commit

---
+++ views/index.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Maple</title>
+    <link rel="stylesheet" href="/public/template.css">
+</head>
+
+<body>
+
+    <nav>
+        <h1>Itai no wa Iya nano de Bougyoryoku</h1>
+    </nav>
+
+    <nav>
+        <img src="/img/Maple-Bolada.jpg">
+        <img src="/img/Maple-Feliz.jpg">
+        <img src="/img/Maple-Pet.jpg">
+    </nav>
+    
+    <nav>
+        <p>Itai no wa Iya nano de Bougyoryoku ni Kyokufuri Shitai to Omoimasu. Todos os Episodios Online, 
+            Assistir Itai no wa Iya nano de Bougyoryoku ni Kyokufuri Shitai to Omoimasu Anime Completo.
+            A história acompanha uma garota que foi convidada para um VRMMORPG pela amiga de escola, mas, 
+            por conta de suas preocupações em sentir dor, acaba criando um personagem totalmente focado em 
+            defesa. Graças a ter colocado todo os seus pontos em VIT (vitalidade) ela conseguiu a skill 
+            especial “Absolute Defense”, que não a deixa tomar nenhum dano, ou sentir dor. A partir daí, 
+            mesmo não sendo capaz de se mover rapidamente, usar magia, e tendo uma força menor que a de 
+            um coelho, Maple começa a se aventurar com seus amigos no mundo do jogo.
+        </p>
+    </nav>
+    
+</body>
+
+</html>
