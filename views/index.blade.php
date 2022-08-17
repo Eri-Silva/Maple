b88d8948840d31c30d859537e3260a69adf2af10
ERIELSON BRITO DA SILVA <silva.erielson@academico.ifrn.edu.br>
2022-08-17 11:15:07 -0300

    oiiiii

--- views/index.blade.php
+++ views/index.blade.php
@@ -12,24 +12,40 @@
         <h1>Itai no wa Iya nano de Bougyoryoku</h1>
     </nav>
 
+    <div class="imagens">
+        <nav>
+            <img src="/img/Maple-Bolada.jpg">
+            <img src="/img/Maple-Feliz.jpg">
+            <img src="/img/Maple-Pet.jpg">
+        </nav>
+    </div>
+    <div class="imagens">
+        <nav>
+            <img src="/img/Maple-Comendo.jpg">
+            <img src="/img/Maple-Clap.jpg">
+            <img src="/img/Maple-Pet.jpg">
+        </nav>
+    </div>
+    <div class="imagens">
+        <nav>
+            <img src="/img/Maple-Bolada.jpg">
+            <img src="/img/Maple-Feliz.jpg">
+            <img src="/img/Maple-Pet.jpg">
+        </nav>
+    </div>
+
     <nav>
-        <img src="/img/Maple-Bolada.jpg">
-        <img src="/img/Maple-Feliz.jpg">
-        <img src="/img/Maple-Pet.jpg">
-    </nav>
-    
-    <nav>
-        <p>Itai no wa Iya nano de Bougyoryoku ni Kyokufuri Shitai to Omoimasu. Todos os Episodios Online, 
+        <p>     Itai no wa Iya nano de Bougyoryoku ni Kyokufuri Shitai to Omoimasu. Todos os Episodios Online,
             Assistir Itai no wa Iya nano de Bougyoryoku ni Kyokufuri Shitai to Omoimasu Anime Completo.
-            A história acompanha uma garota que foi convidada para um VRMMORPG pela amiga de escola, mas, 
-            por conta de suas preocupações em sentir dor, acaba criando um personagem totalmente focado em 
-            defesa. Graças a ter colocado todo os seus pontos em VIT (vitalidade) ela conseguiu a skill 
-            especial “Absolute Defense”, que não a deixa tomar nenhum dano, ou sentir dor. A partir daí, 
-            mesmo não sendo capaz de se mover rapidamente, usar magia, e tendo uma força menor que a de 
+            A história acompanha uma garota que foi convidada para um VRMMORPG pela amiga de escola, mas,
+            por conta de suas preocupações em sentir dor, acaba criando um personagem totalmente focado em
+            defesa. Graças a ter colocado todo os seus pontos em VIT (vitalidade) ela conseguiu a skill
+            especial “Absolute Defense”, que não a deixa tomar nenhum dano, ou sentir dor. A partir daí,
+            mesmo não sendo capaz de se mover rapidamente, usar magia, e tendo uma força menor que a de
             um coelho, Maple começa a se aventurar com seus amigos no mundo do jogo.
         </p>
     </nav>
-    
+
 </body>
 
 </html>
